2870de99ea7ebb84a92b613a780cf0bd0d39de5f
saliy <showlookpro@gmail.com>
2026-01-15 13:16:40 +0800

    chore: update config

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -7,14 +7,14 @@ const CONFIG = {
 
   // 英雄区块导航
   PROXIO_HERO_ENABLE: true, // 开启英雄区
-  PROXIO_HERO_TITLE_1: '开源且免费的基于 Notion 笔记的网站构建工具', // 英雄区文字
-  PROXIO_HERO_TITLE_2: '通过笔记无感知地建站、成倍放大您的价值', // 英雄区文字
+  PROXIO_HERO_TITLE_1: '射频抗衰新宠，风靡明星圈的抗衰答案', // 英雄区文字
+  PROXIO_HERO_TITLE_2: '温和不打扰，改变却可见：轮廓更清楚，肤质更平滑，气色更明亮', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
-  PROXIO_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
+  PROXIO_HERO_BUTTON_1_TEXT: '中国区官网', // 英雄区按钮
   PROXIO_HERO_BUTTON_1_URL:
-    'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
-  PROXIO_HERO_BUTTON_2_TEXT: '在Github上关注', // 英雄区按钮
-  PROXIO_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
+    '#', // 英雄区按钮
+  PROXIO_HERO_BUTTON_2_TEXT: '了解仪器原理', // 英雄区按钮
+  PROXIO_HERO_BUTTON_2_URL: '#', // 英雄区按钮
   PROXIO_HERO_BUTTON_2_ICON: '/images/starter/github-mark.svg', // 英雄区按钮2的图标，不需要则留空
 
   // 英雄区配图，如需隐藏，改为空值即可 ''
