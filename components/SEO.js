c755a721b53fe2410e1fe85a8c0feae5e943c1ee
saliy <showlookpro@gmail.com>
2026-01-17 20:07:58 +0800

    ok

--- components/SEO.js
+++ components/SEO.js
@@ -295,7 +295,7 @@ const getSEOMeta = (props, router, locale) => {
   switch (router.route) {
     case '/':
       return {
-        title: `${siteInfo?.title} | ${siteInfo?.description}`,
+        title: `${siteInfo?.title || TITLE}`,
         description: `${siteInfo?.description}`,
         image: `${siteInfo?.pageCover}`,
         slug: '',
